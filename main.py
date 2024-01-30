b2df0bb0f8397992550137f65513b7f9eaf35061
PedroKBrant <pkb@cin.ufpe.br>
2024-01-30 11:28:26 -0300

    Fix anonymize_directory method by updaing file path

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from lib.transform.facial_landmarks_478_transformer import FacialLandmarks478
 from lib.transform.pix2pix_transformer import Pix2PixTransformer
 from lib.utils import glob_dir, get_last_ckpt, move_files
 
+
 SEED = 42
 
 
@@ -191,7 +192,6 @@ def anonymize_directory(model_file: str, input_directory: str, output_directory:
         input_file = os.path.join(input_directory, file)
         output_file = os.path.join(output_directory, os.path.basename(file))
         anonymize_image(model_file, input_file, output_file, img_size, align, device)
-
 
 if __name__ == '__main__':
     fire.Fire()
